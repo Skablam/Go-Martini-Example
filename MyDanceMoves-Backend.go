b6435dacf2b8df256ae1540b865cccf16ef6ad69
Skablam <stainlesssteelmatt@gmail.com>
2014-06-03 23:41:19 +0100

    first stab at martini backend

--- MyDanceMoves-Backend.go
+++ MyDanceMoves-Backend.go
@@ -2,52 +2,34 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
-	"github.com/ant0ine/go-json-rest"
+	"github.com/go-martini/martini"
 	_ "github.com/lib/pq"
 	"net/http"
+	"os"
 )
 
-type move struct {
-	Name       string
-	Category   string
-	Tags       string
-	Video_link string
-}
-
-type get_move_list_resp struct {
-	Moves []move
-}
-
 type user struct {
-	Username      string
-	Password_hash string
-	Email         string
+	Id       string
+	Password string
+	Email    string
 }
 
-func get_db_connection() *sql.DB {
-
-	dbpool, err := sql.Open("postgres", "dbname=MyDanceMoves user=move_admin password=password sslmode=disable")
-
-	err = dbpool.Ping()
-
-	if err != nil {
-		panic(err.Error())
-	}
-
-	return dbpool
-
+type get_user_resp struct {
+	User   user
+	Result string
 }
 
-func register_user(w *rest.ResponseWriter, req *rest.Request) {
+func RegisterUser(res http.ResponseWriter, req *http.Request, db *sql.DB) {
 
 	newuser := user{}
 
-	err := req.DecodeJsonPayload(&newuser)
+	decoder := json.NewDecoder(req.Body)
 
+	err := decoder.Decode(&newuser)
 	if err != nil {
-		rest.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		panic(err.Error())
 	}
 
 	var insert_statement string = "insert into user_accounts (username, password_hash, email) values ($1, $2, $3)"
@@ -58,20 +40,71 @@ func register_user(w *rest.ResponseWriter, req *rest.Request) {
 		panic(err.Error())
 	}
 
-	res, err := stmt.Exec(newuser.Username, newuser.Password_hash, newuser.Email)
+	queryResult, err := stmt.Exec(newuser.Id, newuser.Password, newuser.Email)
 
-	if err != nil || res == nil {
+	if err != nil || queryResult == nil {
 		panic(err.Error())
 	}
 
 	stmt.Close()
 }
 
-func get_move_list(w *rest.ResponseWriter, req *rest.Request) {
+func GetUser(res http.ResponseWriter, req *http.Request, parms martini.Params, db *sql.DB) {
+
+	username := parms["username"]
+
+	row := db.QueryRow("SELECT trim(email), password_hash FROM user_accounts WHERE username = $1", username)
+
+	var result, email, password_hash string
+
+	var user_info user
+
+	err := row.Scan(&email, &password_hash)
+
+	if err != nil {
+		fmt.Printf("rows.Scan error: %v\n", err)
+		result = "no user found"
+	} else {
+		user_info = user{username, password_hash, email}
+		result = "user found"
+	}
+
+	resp := new(get_user_resp)
+	resp.User = user_info
+	resp.Result = result
+
+	//convert resp struct to json
+	jsonResp, encError := json.Marshal(resp)
+	if encError != nil {
+		fmt.Println(encError)
+		return
+	}
+
+	res.Header().Set("Content-Type", "application/json")
+
+	res.Write(jsonResp)
+
+}
+
+type move struct {
+	Name       string
+	Category   string
+	Tags       string
+	Video_link string
+	Username   string
+}
+
+type get_move_list_resp struct {
+	Moves []move
+}
+
+func GetMoveList(res http.ResponseWriter, req *http.Request, db *sql.DB) {
+
+	username := req.FormValue("username")
 
 	var move_list []move
 
-	rows, statementError := db.Query("SELECT trim(name), trim(category), trim(tags), trim(video_link) FROM moves")
+	rows, statementError := db.Query("SELECT trim(name), trim(category), trim(tags), trim(video_link) FROM moves WHERE username = $1", username)
 
 	if statementError != nil {
 		panic(statementError)
@@ -91,7 +124,7 @@ func get_move_list(w *rest.ResponseWriter, req *rest.Request) {
 			fmt.Printf("rows.Scan error: %v\n", err)
 		}
 
-		current_move := move{name, category, tags, video_link}
+		current_move := move{name, category, tags, video_link, username}
 
 		move_list = append(move_list, current_move)
 
@@ -99,19 +132,93 @@ func get_move_list(w *rest.ResponseWriter, req *rest.Request) {
 
 	resp := new(get_move_list_resp)
 	resp.Moves = move_list
-	w.WriteJson(&resp)
+
+	//convert resp struct to json
+	jsonResp, encError := json.Marshal(resp)
+	if encError != nil {
+		fmt.Println(encError)
+		return
+	}
+
+	res.Header().Set("Content-Type", "application/json")
+
+	res.Write(jsonResp)
 }
 
-var db *sql.DB //global db pool connection
+func AddMove(res http.ResponseWriter, req *http.Request, db *sql.DB) {
+
+	decoder := json.NewDecoder(req.Body)
+
+	newmove := move{}
+
+	err := decoder.Decode(&newmove)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	var insert_statement string = "insert into moves (name, category, tags, video_link, username) values ($1, $2, $3, $4, $5)"
+
+	stmt, err := db.Prepare(insert_statement)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	queryResult, err := stmt.Exec(newmove.Name, newmove.Category, newmove.Tags, newmove.Video_link, newmove.Username)
+
+	if err != nil || queryResult == nil {
+		panic(err.Error())
+	}
+
+	stmt.Close()
+
+	res.Write([]byte("Move added"))
+}
+
+func get_db_connection() *sql.DB {
+
+	dbuser := os.Getenv("DBUSER")
+	dbname := os.Getenv("DBNAME")
+	dbpass := os.Getenv("DBPASS")
+
+	dbconn, err := sql.Open("postgres", "dbname="+dbname+" host=localhost port=5432 user="+dbuser+" password="+dbpass+" sslmode=disable")
+
+	err = dbconn.Ping()
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return dbconn
+
+}
+
+// DB Returns a martini.Handler
+//func DB() martini.Handler {
+//	session, err := get_db_connection()
+//	if err != nil {
+//		panic(err)
+//	}
+
+//	return func(c martini.Context) {
+//		s := session.Clone()
+//		c.Map(s.DB("advent"))
+//		defer s.Close()
+//		c.Next()
+//	}
+//}
+
+var dbpool *sql.DB //global db pool connection
 
 func main() {
 
-	db = get_db_connection()
+	dbpool = get_db_connection()
 
-	handler := rest.ResourceHandler{}
-	handler.SetRoutes(
-		rest.Route{"GET", "/movelist", get_move_list},
-		rest.Route{"POST", "/registeruser", register_user},
-	)
-	http.ListenAndServe(":8081", &handler)
+	m := martini.Classic()
+	m.Map(dbpool)
+	m.Get("/movelist", GetMoveList)
+	m.Get("/getuser/:username", GetUser)
+	m.Post("/addmove", AddMove)
+	m.Post("/registeruser", RegisterUser)
+	m.Run()
 }
